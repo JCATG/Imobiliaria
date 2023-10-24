eaaaa3762de1d892127e59fb8fe72ee5c6c3c50a
JCATG <j.alves160600@gmail.com>
2023-10-24 10:03:05 -0300

    <Fix>:Adjusting responsive in page negocie seu imovel

--- page-negocie-imovel.php
+++ page-negocie-imovel.php
@@ -19,7 +19,7 @@
 <?php get_header(); ?>
 <div class="bg-graypage">
     <div class="w-full max-w-5xl mx-auto">
-        <div class="flex-col flex lg:flex flex-wrap gap-6">
+        <div class="mx-3 flex-col flex lg:mx-0 lg:flex flex-wrap gap-6">
             <p class="text-2xl mt-4">Informe abaixo suas Informações</p>
             <?php
             if (isset($_SESSION['mensagem'])) {
@@ -29,23 +29,27 @@
             ?>
             <a name="form-anchor"></a>
             <form method="post">
-            <input type="text" placeholder="Nome" class="w-3/5 border p-2" name="name">
-            <input type="text" placeholder="Celular" class="border w-2/6 mt-2 p-2" name="form_cel">
-            <input type="text" placeholder="Email" class="w-3/5 border mt-2 p-2" name="email">
-            <input type="text" placeholder="Cidade/Estado que reside" class="border w-2/6 p-2" name="cidade">
-            <div class="mt-14">
-                <p class="text-2xl mb-2">Informações do Imovel</p>
-                <select name="tipo_imovel" required class="p-3 text-md">
-                    <option value="Apartamento">Apartamento</option>
-                    <option value="Casa">Casa</option>
-                </select>
+                <div class="w-full md:w-full lg:mx-0">
+                    <input type="text" placeholder="Nome" class="w-full md:w-3/5 border p-2" name="name">
+                    <input type="text" placeholder="Celular" class="border w-full md:w-2/6 mt-2 p-2" name="form_cel">
+                    <input type="text" placeholder="Email" class="w-full md:w-3/5 border mt-2 p-2" name="email">
+                    <input type="text" placeholder="Cidade" class="border mt-2 w-full md:w-2/6 p-2 md:mt-0" name="cidade">
+                </div>
+                <div class="mt-14">
+                    <p class="text-2xl mb-2">Informações do Imovel</p>
+                    <div class="flex flex-col md:flex-row md:gap-3">
+                        <select name="tipo_imovel" required class="p-3 text-md">
+                            <option value="Apartamento">Apartamento</option>
+                            <option value="Casa">Casa</option>
+                        </select>
 
-                    <select name="finalidade" required class="p-3 text-md w-1/5">
-                        <option value="venda">Venda</option>
-                        <option value="locação">Locação</option>
-                        <option value="Temporada">Temporada</option>
-                    </select>
-                    <input type="text" placeholder="Cidade" class="w-3/5 border p-2" name="cidade_imovel">
+                        <select name="finalidade" required class="w-full p-3 mt-2 text-md md:mt-0 md:w-1/5">
+                            <option value="venda">Venda</option>
+                            <option value="locação">Locação</option>
+                            <option value="Temporada">Temporada</option>
+                        </select>
+                    </div>
+                    <input type="text" placeholder="Cidade" class="w-full mt-2 md:w-full md:t-0 border p-2" name="cidade_imovel">
                     <textarea cols="30" rows="5" class="w-full border mt-2" placeholder="Descrição do imovel" name="mensagem"></textarea>
                     <p>Envie Fotos do seu imovel</p>
                     <input type="file" name="file" class="text-sm bg-white text-black" />
